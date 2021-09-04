3d8b7c38c7918fa2b1ae14aad4807777a50ddd9b
Sebastian Thomas <64014678+stho382@users.noreply.github.com>
2021-09-04 06:05:12 +0000

    edited graphs

--- ODE_Model_Function.py
+++ ODE_Model_Function.py
@@ -520,9 +520,15 @@ def plot_model(Future_Productions, Future_Time, Labels):
         )
         (HandlesP[i], ) = axP.plot(tPset[i], xPset[i], colours[i%6] + "-")
     
+    stakeholder_labels = ['ROTORUA CITY COUNCIL', 'TŪHOURANGI NGĀTI WĀHIAO', 'LOCAL CHAMBER OF COMMERCE']
+
+    for stakeholder_P in range(len(stakeholder_labels)):
+        axP.text(tPset[stakeholder_P][-1], xPset[stakeholder_P][-1] - 105000,stakeholder_labels[stakeholder_P], horizontalalignment = 'right', verticalalignment='bottom', fontsize=7, fontweight='bold')
+
     axP.legend(handles = HandlesP, labels = Labels)
     plt.title(label = 'Pressure')
-    plt.show()
+    figP.savefig("Pressure.png")
+    plt.close(figP)
 
 
     #NOW PLOTTING TEMPERATURE
@@ -558,9 +564,13 @@ def plot_model(Future_Productions, Future_Time, Labels):
         )
         (HandlesT[i], ) = axT.plot(tTset[i], xTset[i], colours[i%6] + "-")
     
+    for stakeholder_T in range(len(stakeholder_labels)):
+      axT.text(tTset[stakeholder_P][-1], xTset[stakeholder_P][-1] - 0.01,stakeholder_labels[stakeholder_P], horizontalalignment = 'right', verticalalignment='bottom', fontsize=7, fontweight='bold')  
+    
     axT.legend(handles = HandlesT, labels = Labels)
     plt.title(label = 'Temperature')
-    plt.show()
+    figT.savefig("Temperature.png")
+    plt.close(figT)
 
 if __name__ == "__main__":
     Future_Productions = [10000, 0, 20000]
--- test_functions.py
+++ test_functions.py
@@ -20,12 +20,12 @@ def test_solve_temperature_ode():
     """    
     # test for when P0> P
     dtdt1 = [1, 1.3125 , 1.5078125]
-    T1, dTdt1 = solve_temperature_ode(ode_temperature_model,0, 1, 0.5, 1, [0,0,1,2,3,4,5,6,7])
+    T1, dTdt1 = solve_temperature_ode(ode_temperature_model,0, 1, 0.5, 1, [0.,0.,1.,2.,3.,4.,5.,6.,7.])
 
     # test for when P0 < P
     
-    dtdt2 = [1, 62.88746875, 101.56713672])
+    dtdt2 = [1, 62.88746875, 101.56713672]
     T2, dTdt2 = solve_temperature_ode(ode_temperature_model, 0, 1, 0.5, 1, [0,0,1,2,3,4,5,7,6])
-
+    
     assert norm(dTdt1 - dtdt1) < 1.e-10
     assert norm(dTdt2 - dtdt2) < 1.e-10
