030d36fb1cfc535ad82d5c16bc319440ec7ec800
toco314 <toco314@aucklanduni.ac.nz>
2021-09-04 19:04:08 +1200

    Added unit tests for temperature ode

--- test_functions.py
+++ test_functions.py
@@ -15,3 +15,26 @@ def test_solve_pressure_ode():
     assert abs(dpdt[1] - dPdt[1]) < 1.e-4
     assert abs(dpdt[2] - dPdt[2]) < 1.e-4
 
+
+def test_solve_temperature_ode():
+    '''
+	Tests if function ode_temperature_model is working properly by comparing it with a known result.
+	'''
+    # test for when P0> P
+    dtdt1 = [1, 1.3125 , 1.5078125]
+    T1, dTdt1 = solve_temperature_ode(ode_temperature_model,0, 1, 0.5, 1, [0,0,1,2,3,4,5,6,7])
+
+    # test for when P0 < P
+    
+    dtdt2 = [1, 62.88746875, 101.56713672]
+    T2, dTdt2 = solve_temperature_ode(ode_temperature_model, 0, 1, 0.5, 1, [0,0,1,2,3,4,5,7,6])
+
+    # Asserts for when P0 > P 
+    assert abs(dTdt1[0] - dtdt1[0]) < 1.e-4
+    assert abs(dTdt1[1] - dtdt1[1]) < 1.e-4
+    assert abs(dTdt1[2] - dtdt1[2]) < 1.e-4
+
+    # Asserts for when P0 < P 
+    assert abs(dTdt2[0] - dtdt2[0]) < 1.e-4
+    assert abs(dTdt2[1] - dtdt2[1]) < 1.e-4
+    assert abs(dTdt2[2] - dtdt2[2]) < 1.e-4
