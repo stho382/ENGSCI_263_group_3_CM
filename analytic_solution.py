3632f7138cf8ec2ef03cc6561a89487b8d65d500
stho382 <stho382@aucklanduni.ac.nz>
2021-09-10 11:29:40 +1200

    moved analytic_solution.py to home directory

---
+++ analytic_solution.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Pressure
+def pressure_analytic(t, P, q, dqdt, P0, ap, bp, cp):
+    # returns analytic solution P for a constant production rate q
+
+    return -ap * q / bp * (1 - np.exp(-bp * t)) + P0
+
+
+# Temperature
+def temperature_analytic(t, T, Tc, T0, at, bt, ap, bp, P, P0, q):
+    # returns analytic solution for temperature ODE with constant q and negligible conduction
+
+    return Tc + (T0 - Tc) * np.exp((-at * q / bp * (np.exp(-bp * t) + bp * t - 1)))
